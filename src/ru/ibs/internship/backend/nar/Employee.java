0c512bddc8d8221d5712ec0ad074560a5712eeb3
Azaliya <Mystery888m@yandex.ru>
2021-06-03 14:05:59 +0500

    1) добавлено поле "positionName" с сеттером и геттером; 2) добавлен ещё один конструктор; 3) добавлен метод "printWorkExperience".

--- src/ru/ibs/internship/backend/nar/Employee.java
+++ src/ru/ibs/internship/backend/nar/Employee.java
@@ -1,15 +1,26 @@
 package ru.ibs.internship.backend.nar;
 
 public abstract class Employee {
+
+    private String positionName;
     private String name;
     private String surname;
-    private Integer workExperience;
+    private Integer workExperience; // в годах
     private Double salary;
 
     public Employee() {
     }
+    public Employee(String name, String surname, Integer workExperience) {
+        this.setName(name);
+        this.setSurname(surname);
+        this.setWorkExperience(workExperience);
+    }
 
-    public void printInfo() {}
+    public abstract void printInfo();
+    public void printWorkExperience() {
+        System.out.printf("Сотрудник %s %s на должности %s имеет опыт работы %d (года/лет).%n"
+                            , this.getName(), this.getSurname(), this.getPositionName(), this.getWorkExperience());
+    }
 
     public void setName(String name) {
         this.name = name;
@@ -23,6 +34,9 @@ public abstract class Employee {
     public void setSalary(Double salary) {
         this.salary = salary;
     }
+    public void setPositionName(String positionName) {
+        this.positionName = positionName;
+    }
 
     public String getName() {
         return this.name;
@@ -35,6 +49,16 @@ public abstract class Employee {
     }
     public Double getSalary() {
         return this.salary;
+    }
+    public String getPositionName() {
+        if (this instanceof Trainee) {
+            this.positionName = "\"cтажёр\"";
+        } else if (this instanceof Developer) {
+            this.positionName = "\"разработчик\"";
+        } else if (this instanceof TeamLead) {
+            this.positionName = "\"TeamLead\"";
+        }
+        return this.positionName;
     }
 
 }
